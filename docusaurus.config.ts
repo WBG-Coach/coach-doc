bb21f9f8d3b5d15a0ebee678142bc2df3685a714
John Moravec <51122406+educationfutures@users.noreply.github.com>
2023-12-21 07:58:42 -0600

    copyright update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -124,7 +124,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} The World Bank Group. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
